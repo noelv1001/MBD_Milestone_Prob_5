8e238d5138b1670e6a6e6c739d22503e5ff617b9
Noel Varghese <Noel Varghese@Noel>
2020-12-08 10:52:20 +0530

    Added few comments. Observed the output in data inspector.

--- Initial_Cond.m
+++ Initial_Cond.m
@@ -1,7 +1,10 @@
+clearvars
+%Initializing resistence and capacitance values
 R=10;
 C=0.00001;
-
+q=1;
+%initial condition for loop
 for i=1:5
-    q=i*2*10^-3
+    q=i*2*(10^-5);
     sim('MBD_Prob5.slx');
 end
